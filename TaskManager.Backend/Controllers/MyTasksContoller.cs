17e6876695f46a2d7deebd79b7929d337740690e
Sebastian <Sebastianbrand046@outlook.com>
2023-11-27 16:39:40 -0500

    Add DataContext and controller

---
+++ TaskManager.Backend/Controllers/MyTasksContoller.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TaskManager.Backend.Data;
+using TaskManager.Shared.Entities;
+
+namespace TaskManager.Backend.Controllers
+{
+    [Route("api/mytasks")]
+    [ApiController]
+    public class MyTasksContoller : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public MyTasksContoller(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.MyTasks.ToList());
+        }
+
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            var task = _context.MyTasks.FirstOrDefault(x => x.Id == id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public IActionResult Post(MyTask myTask)
+        {
+            _context.MyTasks.Add(myTask);
+            _context.SaveChanges();
+            return Ok(myTask);
+        }
+
+        [HttpPut]
+        public IActionResult Put(MyTask myTask)
+        {
+            var task = _context.MyTasks.FirstOrDefault(x => x.Id == myTask.Id);
+
+            if (task == null)
+            {
+                NotFound();
+            }
+
+            _context.MyTasks.Update(task!);
+            _context.SaveChanges();
+
+            return Ok(task);
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var task = _context.MyTasks.FirstOrDefault(x => x.Id == id);
+
+            if (task == null)
+            {
+                NotFound();
+            }
+
+            _context.MyTasks.Remove(task!);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
--- TaskManager.Backend/Program.cs
+++ TaskManager.Backend/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Backend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer("name=DefaultConnection"));    
 
 var app = builder.Build();
 
